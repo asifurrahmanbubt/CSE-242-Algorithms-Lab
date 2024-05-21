0c992d91427da67dca922a0b80937c9b6e6adb21
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2024-05-21 17:46:27 +0600

    Create Implement the BFS Algorithm.cpp

---
+++ Implement the BFS Algorithm.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
+
+class Graph {
+    int V;
+    list<int> *adj;
+public:
+    Graph(int V);
+    void addEdge(int v, int w);
+    void BFS(int s);
+};
+
+Graph::Graph(int V) {
+    this->V = V;
+    adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w) {
+    adj[v].push_back(w);
+}
+
+void Graph::BFS(int s) {
+    vector<bool> visited(V, false);
+    list<int> queue;
+
+    visited[s] = true;
+    queue.push_back(s);
+
+    while (!queue.empty()) {
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
+
+        for (auto adjecent : adj[s]) {
+            if (!visited[adjecent]) {
+                visited[adjecent] = true;
+                queue.push_back(adjecent);
+            }
+        }
+    }
+}
+
+int main() {
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    cout << "Breadth First Traversal (starting from vertex 2) \n";
+    g.BFS(2);
+
+    return 0;
+}
