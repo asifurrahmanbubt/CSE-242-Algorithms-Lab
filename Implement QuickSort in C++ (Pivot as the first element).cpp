ccdcad2853e4bea0ab0e0376d62fa72d44358f2e
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2024-05-21 17:44:04 +0600

    Create Implement QuickSort in C++ (Pivot as the first element).cpp

---
+++ Implement QuickSort in C++ (Pivot as the first element).cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low];
+    int left = low + 1;
+    int right = high;
+
+    while (true) {
+        while (left <= right && arr[left] <= pivot)
+            left++;
+
+        while (left <= right && arr[right] >= pivot)
+            right--;
+
+        if (left > right)
+            break;
+
+        swap(&arr[left], &arr[right]);
+    }
+
+    swap(&arr[low], &arr[right]);
+    return right;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n - 1);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
